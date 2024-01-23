eb4fb98c947448c9878eb3e10061c3b28b2180c9
Duwayne <102155618+DFBlok@users.noreply.github.com>
2024-01-23 10:14:56 +0200

    Indexing and methods

---
+++ string-indexing-and-methods/dare-to-decipher.js
@@ -0,0 +1,18 @@
+let pharse = "that's all folks";
+console.log(pharse[pharse.length]);
+console.log(pharse[pharse.length -1]);
+console.log(pharse[pharse.length -2 ]);
+console.log("-----------------------------");
+
+const i =9;
+const char = pharse[i];
+console.log(char);
+console.log(pharse.indexOf(char));
+console.log(pharse.slice(2, 8));
+console.log("-------------------------------");
+
+console.log("abcdefg".slice(1,3));
+console.log("abcdefg".slice(2));
+console.log("abcdefg".slice(4));
+console.log("abcdefg".slice(2,-1));
+console.log("abcdefg".slice(2,-2));
