2ae5847f51576e7964e379f639eb0c4e50ee6c6a
Duwayne <102155618+DFBlok@users.noreply.github.com>
2024-01-24 12:02:26 +0200

    functions exercise 1 and 2

---
+++ A_functions_exercise/snippet-0-1.js
@@ -0,0 +1,17 @@
+//snippet 0-1
+console.log("=======snippet 0-1=======")
+let greet = function(){
+    console.log("hey");
+    console.log("programmers");
+};
+
+let whistle = function(){
+    console.log("doot");
+}
+
+console.log("First");
+console.log("Second");
+greet();
+console.log("Third");
+console.log("Fourth");
+whistle(); 
---
+++ A_functions_exercise/snippet-0-2.js
@@ -0,0 +1,18 @@
+//snippet  0-2
+console.log("====snippet 0-2======")
+let howMany = function(){
+    return 42;
+};
+
+console.log(howMany);
+console.log(howMany());
+
+console.log("================")
+const theAnswer = howMany();
+console.log(theAnswer);
+
+console.log("================")
+let howMuch = function () {
+    5;
+  };
+console.log(howMuch());
