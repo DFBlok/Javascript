7d826eb3dbd374eb327bf36e8b5fcee91f2af3b4
Duwayne <102155618+DFBlok@users.noreply.github.com>
2024-01-25 11:46:44 +0200

    exercise B started

---
+++ B_functions_exercise/start-with-r.js
@@ -0,0 +1,8 @@
+let startsWithR = function(str){
+    return str.charAt(0).toLowerCase() === "r";
+}
+
+console.log(startsWithR("rogger that"));
+console.log(startsWithR("Row, row,row your boat"));
+console.log(startsWithR("slip"));
+console.log(startsWithR("Taxicab"));
