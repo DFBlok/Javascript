d030d8864031645169ca5886634c5495238c78aa
Duwayne <102155618+DFBlok@users.noreply.github.com>
2024-01-25 14:30:01 +0200

    exercise C done

---
+++ C_functions_exercise/larger.js
@@ -0,0 +1,9 @@
+let larger = function(n1, n2){
+    return Math.max(n1, n2);
+}
+
+console.log(larger(256, 400));
+console.log(larger(31, 4));        
+console.log(larger(-6, 7));        
+console.log(larger(11.3, 11.2));   
+console.log(larger(-10, -3));      
