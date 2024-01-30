3ed201bb68db882652e67842dfd234d5e6cdd9c4
unknown <duwayneblok01@gmail.com>
2024-01-30 12:29:51 +0200

    B Loop exercise update 1

---
+++ LOOPS/B_loops_exercise/five-multiples-of.js
@@ -0,0 +1,10 @@
+let fiveMultiplesOf = function(num){
+    for (let i=1; i <= 5; i++){
+       console.log(num * i);
+
+    }
+}
+
+fiveMultiplesOf(7);
+console.log("---------------")
+fiveMultiplesOf(3);
