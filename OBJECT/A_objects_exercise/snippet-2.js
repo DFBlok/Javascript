9cf5e0735edb8f0664bb148d2153d3fe01e81ac9
DFBlok <duwayneblok01@gmail.com>
2024-02-07 13:41:10 +0200

    Object exercises B

---
+++ OBJECT/A_objects_exercise/snippet-2.js
@@ -0,0 +1,18 @@
+let restaurant = {
+    name: "Bob's Burgers",
+    location: "123 Ocean Avenue",
+    owners: ["Bob Belcher", "Linda Belcher"],
+    established: 2011,
+    menu: ["burgers", "fries", "shakes"],
+  };
+  
+  console.log("owners" in restaurant);
+  console.log("employees" in restaurant);
+
+let someKey = "menu";
+console.log(someKey in restaurant);
+
+console.log(restaurant.menu);
+console.log(restaurant["menu"]);
+console.log(restaurant[someKey]); //
+console.log(restaurant.someKey); // looks for a property "someKey" in restaurant onject ==undefined
