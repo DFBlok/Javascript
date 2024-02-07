233e40ef8315d67d1e3be76d2e9a7f41d0eb8b40
DFBlok <duwayneblok01@gmail.com>
2024-02-07 14:01:40 +0200

    Object exercises C

--- OBJECT/A_objects_exercise/snippet-3.js
+++ OBJECT/A_objects_exercise/snippet-3.js
@@ -4,20 +4,22 @@ let dog = {
     breed: "pug",
     color: "fawn",
     favoriteFoods: ["bacon"],
+    car: "Volkwagen",
 };
 
 console.log(dog.age);
 console.log(dog.breed);
 console.log(dog.favoriteFoods);
-
+console.log("------------------------------");
 dog.age++;
 dog.breed = dog.breed.toUpperCase();
 dog.favoriteFoods.push("sausage");
-
+console.log("---------------------------");
 console.log(dog.age);
 console.log(dog.breed);
 console.log(dog.favoriteFoods);
-
+console.log("------------------------------");
+//print everything inside the dog object
 for (let property in dog){
     console.log(property + " is " + dog[property]);
 }
