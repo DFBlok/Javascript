d831281ececf3958688a22dc005cf4ee87037448
DFBlok <s219392862@mandela.ac.za>
2024-02-19 14:43:56 +0200

    Decomposition - Map examples

---
+++ DECOMPOSITION/EXAMPLES_Map/map.js
@@ -0,0 +1,15 @@
+let rad = [2, 3, 4, 5];
+
+let result = rad.map(function (r){
+    return r * 2;
+});
+
+console.log(result);
+
+// Extra example
+const num = [6, 7,8,10];
+const doouble = num.map(function (item){
+    return item * 2;
+});
+console.log(doouble); 
+ 
---
+++ DECOMPOSITION/EXAMPLES_Map/squareNumbers.js
@@ -0,0 +1,7 @@
+const n = [1,2,3,4,5];
+
+const squareNo = n.map(function (num){
+    return num * num;
+});
+
+console.log(squareNo);
